34583ef8e2ad9a1bfd6800779e98ee91f80cef27
skmbw <tongku2008@126.com>
2019-10-09 10:18:49 +0800

    代买错误改进，使用python3.5

--- aimijia.py
+++ aimijia.py
@@ -1,44 +1,46 @@
 import aliyun.api
-import urllib2
+import urllib.request
 import json
 
+
 class DNS:
-    jsonfile = file('key.json')
-    s = json.load(jsonfile)
-    aliyun.setDefaultAppInfo(str(s['id']),str(s['secret']))
+    jsonFile = open('key.json')
+    s = json.load(jsonFile)
+    aliyun.setDefaultAppInfo(str(s['id']), str(s['secret']))
+
     def getDNSIp(self):
         b = aliyun.api.Dns20150109DescribeDomainRecordInfoRequest()
         try:
             f = b.getResponse()
-            return (str(f.get('Value')))
-        except Exception,e:
-            print('getDNSIp:',e)
+            return str(f.get('Value'))
+        except Exception as e:
+            print('getDNSIp:', e)
             return None
 
     def getMyIp(self):
         try:
-            u = urllib2.urlopen('http://members.3322.org/dyndns/getip')
+            u = urllib.request.urlopen('http://members.3322.org/dyndns/getip')
             return u.read().strip('\n')
-        except HTTPError as e:
-            print('getMyIp:',e)
-            return None;
+        except Exception as e:
+            print('getMyIp:', e)
+            return None
 
-    def main(self,newIp):
-        a = aliyun.api.Dns20150109UpdateDomainRecordRequest(newIp);
+    def main(self, newIp):
+        a = aliyun.api.Dns20150109UpdateDomainRecordRequest(newIp)
         a.DBInstanceId = ""
         try:
             print("start")
-            f = a.getResponse();
+            f = a.getResponse()
             print(f)
-        except Exception , e:
-            print('main:',e)
+        except Exception as e:
+            print('main:', e)
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     d = DNS()
     oldip = d.getDNSIp()
     newip = d.getMyIp()
-    if(oldip != newip and oldip is not None):
-        print('oldIp:',oldip)
-        print('newIp:',newip)
+    if oldip != newip and oldip is not None:
+        print('oldIp:', oldip)
+        print('newIp:', newip)
         d.main(newip)
